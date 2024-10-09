12fd7b83fb7cb4b09104f93a074a0f2571f88341
meviymw <mickeymilkxxx@gmail.com>
2024-10-09 18:26:26 +0300

    Поправила структуру таблицы в БД

--- Uchet_sotrudnikov.sql
+++ Uchet_sotrudnikov.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 07 2024 г., 17:57
+-- Время создания: Окт 09 2024 г., 18:24
 -- Версия сервера: 8.0.30
 -- Версия PHP: 8.1.9
 
@@ -31,8 +31,8 @@ CREATE TABLE `Information_about_sotr` (
   `ID_sotrudnika` int NOT NULL,
   `FIO_sotrudnika` text NOT NULL,
   `Date_birth` date NOT NULL,
-  `Seria_and_nomer_passporta` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Contact_info` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Seria_and_nomer_passporta` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Contact_info` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Adress` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Otdel` text NOT NULL,
   `Doljnost` text NOT NULL,
@@ -46,8 +46,11 @@ CREATE TABLE `Information_about_sotr` (
 --
 
 INSERT INTO `Information_about_sotr` (`ID_sotrudnika`, `FIO_sotrudnika`, `Date_birth`, `Seria_and_nomer_passporta`, `Contact_info`, `Adress`, `Otdel`, `Doljnost`, `Zarplata`, `Date_prin_na_rab`, `Status`) VALUES
-(1, 'Комаров Егор Евгеньевич', '2000-12-02', '7890654321', '+79605809926', 'г. Ярославль, ул. Строительная, 10', 'Отдел кадрового обеспечения', 'Специалист по кадровому обеспечению', 85000, '2022-09-15', 'Работает'),
-(2, 'Трюфелева Ирина Олеговна', '1999-10-10', '9012724556', '+79705347765', 'г. Ярославль, ул. Машиностроителей, 5', 'Отдел по обучению и развитию', 'Специалист по обучению и развитию', 70500, '2024-09-04', 'Работает');
+(1, 'Трюфелева Ирина Олеговна', '1999-10-10', '88 09 451154', '+7(990)-676-67-45', 'г. Ярославль, ул. Машиностроителей, 5', 'Отдел по обучению и развитию', 'Специалист по обучению и развитию', 70500, '2024-09-04', 'Работает'),
+(2, 'Комаров Егор Евгеньевич ', '2000-12-02', '90 65 827386', '+7(999)-688-92-44', 'г. Ярославль, ул. Строительная, 10', 'Отдел кадрового обеспечения', 'Специалист по кадровому обеспечению', 85000, '2020-09-15', 'Работает'),
+(3, 'Токнов Егор Дмитриевич', '2004-09-10', '70 55 632125', '+7(980)-210-33-59', 'г. Ярославль, ул. Саукова, 15', 'Отдел по управлению талантами', 'Специалист по управлению талантами', 80000, '2024-10-01', 'Работает'),
+(4, 'Котова Елена Александровна', '1999-05-12', '65 65 709553', '+7(980)-781-88-99', 'г. Ярославль, ул. Садовая, 33', 'Отдел по благосостоянию сотрудников', 'Специалист по благосостоянию сотрудников', 95000, '2020-05-03', 'Работает'),
+(5, 'Матюхин Сергей Алексеевич', '2001-09-09', '78 55 637482', '+7(988)-665-82-99', 'г. Ярославль, ул. Булочная, 11', 'Отдел по работе с персоналом', 'Специалист по работе с персоналом', 70500, '2024-09-01', 'Работает');
 
 --
 -- Индексы сохранённых таблиц
@@ -58,6 +61,16 @@ INSERT INTO `Information_about_sotr` (`ID_sotrudnika`, `FIO_sotrudnika`, `Date_b
 --
 ALTER TABLE `Information_about_sotr`
   ADD PRIMARY KEY (`ID_sotrudnika`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `Information_about_sotr`
+--
+ALTER TABLE `Information_about_sotr`
+  MODIFY `ID_sotrudnika` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
